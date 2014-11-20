88a881026823ac19c609e7a1eeebb23ac44b3933
johnedelatorre <john.edelatorre@yahoo.com>
2014-11-19 21:01:58 -0500

    flik admin dashboard framework

---
+++ flik_admin_app/flik_admin_app/spec/prod/settings.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from flik_admin_app.settings import *
+
+WSGI_APPLICATION = 'flik_admin_app.spec.prod.wsgi.application'
---
+++ flik_admin_app/flik_admin_app/spec/stage/wsgi.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flik_admin_app.spec.stage.settings")
+
+from flik_admin_app.wsgi import application
